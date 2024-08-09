577ae6824fd97d37b6b9e14b6da9a8867b0587a3
Jesse Brault <jbrault@mac.com>
2024-08-09 07:56:08 -0500

    Fixed RefreshTokenRepository ID to Long.

--- src/main/java/app/mealsmadeeasy/api/auth/RefreshTokenRepository.java
+++ src/main/java/app/mealsmadeeasy/api/auth/RefreshTokenRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
     Optional<RefreshTokenEntity> findByToken(String token);
 }
