862d30fae974135f92a661f24371d87ff16ce688
Jesse Brault <jbrault@mac.com>
2024-08-14 09:29:19 -0500

    Add all methods to cors.

--- src/main/java/app/mealsmadeeasy/api/MvcConfiguration.java
+++ src/main/java/app/mealsmadeeasy/api/MvcConfiguration.java
@@ -15,6 +15,7 @@ public class MvcConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins("http://localhost:5173")
                         .allowCredentials(true);
             }
